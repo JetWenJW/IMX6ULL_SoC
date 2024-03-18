e960129f990184b8e327574a4e703b6e08508021
JetWen <killer500k@gmail.com>
2024-03-18 17:07:07 +0800

    Add GPIO function to 7_key finished

--- B_Drivers/7_key/bsp/beep/bsp_beep.c
+++ B_Drivers/7_key/bsp/beep/bsp_beep.c
@@ -1,26 +1,28 @@
 #include "bsp_beep.h"
 #include "cc.h"
+#include "bsp_gpio.h"
+
 /* Beep initial */
 void beep_init(void)
 {
+    gpio_pin_config_t beep_config;
     IOMUXC_SetPinMux(IOMUXC_SNVS_SNVS_TAMPER1_GPIO5_IO01 ,0);       /* Set GPIo Pin */
     IOMUXC_SetPinConfig(IOMUXC_SNVS_SNVS_TAMPER1_GPIO5_IO01,0x10B0);/* Configure Electronical Property */
 
-    /* GPIO Initial */
-    GPIO5 -> GDIR|= (1 << 1);                                   /* Set GPIO5 as an output  */
-    GPIO5 -> DR &=~ (1 << 1);                                   /* Set Beep Defaut is OFF  */
- 
+    /*  GPIO initial */
+    beep_config.direction = kGPIO_DigitalOutput;
+    beep_config.outputLogic = 1;
+    gpio_init(GPIO5, 1, &beep_config); 
 }
 
 void beep_switch(int status)
 {
     if(status == ON)
     {
-        GPIO5 -> DR &=~ (1 << 1);       /* Clear bit[1] */
+        gpio_pinwrite(GPIO5, 1 ,0);       /* Clear bit[1] */
     }
     else if(status == OFF)
     {
-        
-        GPIO5 -> DR |= (1 << 1);        /* Set bit[1]   */
+        gpio_pinwrite(GPIO5, 1 ,1);        /* Set bit[1]   */
     }
 }
--- B_Drivers/7_key/bsp/key/bsp_key.c
+++ B_Drivers/7_key/bsp/key/bsp_key.c
@@ -1,22 +1,25 @@
 #include "bsp_key.h"
 #include "bsp_delay.h"
+#include "bsp_gpio.h"
 
 
 /* Key initial */
 void key_init(void)
 {
+    gpio_pin_config_t key_config;
     IOMUXC_SetPinMux(IOMUXC_UART1_CTS_B_GPIO1_IO18 ,0);           /* Set Pin as GPIO_IO18  */
     IOMUXC_SetPinConfig(IOMUXC_UART1_CTS_B_GPIO1_IO18,0xF080);    /* Configure Electronical Property */
 
-    /* GPIO Initial */
-    GPIO1 -> GDIR &=~ (1 << 18);                                   /* Set GPIO18 as an input  */
+    /*  GPIO initial */
+    key_config.direction = kGPIO_DigitalInput;
+    gpio_init(GPIO1, 18, &key_config);
 }
 
 /* Read the Key Value 0:Press/1:Release */
 int read_key(void)
 {
     int ret = 0;
-    ret = ((GPIO1 -> DR) >> 18) & 0x01;
+    ret = gpio_pinread(GPIO1, 18);
     return ret;
 }
 
@@ -25,16 +28,16 @@ int key_getvalue(void)
     int ret = 0;
     static unsigned char release = 1;  /* KeyValue = 1 means Release */
 
-    if((release == 1) && (read_key() == 0))/* Press key */
+    if((release == 1) && (gpio_pinread(GPIO1, 18) == 0))/* Press key */
     {
         delay(10);                            /* delay 10ms */
         release = 0;
-        if(read_key() == 0)                   /* Read Key Value */
+        if(gpio_pinread(GPIO1, 18) == 0)                   /* Read Key Value */
         {
             ret = KEY0_VALUE;
         }
     }
-    else if(read_key() == 1)
+    else if(gpio_pinread(GPIO1, 18) == 1)
     {
         ret = KEY_NONE;
         release = 1;
